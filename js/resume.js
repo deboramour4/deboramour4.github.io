767b735901e02cf13778f87c5b03ad8740904745
Débora Moura <deboramoura@alu.ufc.br>
2019-01-27 18:05:29 -0300

    Projects

--- js/resume.js
+++ js/resume.js
@@ -70,5 +70,15 @@
     }
   })
 
+  // Project Gallery ----------------------------------------------
+  $('.md-trigger').on('click', function() {
+    $('.md-modal').addClass('md-show');
+  });
+  
+  $('.md-close').on('click', function() {
+    $('.md-modal').removeClass('md-show');
+  });
+
+  
 })(jQuery); // End of use strict
 
